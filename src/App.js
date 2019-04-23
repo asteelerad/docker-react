e6718023b18a12f7ce48f3821c3fbb0f85964386
Andrew Steele <asteele@radiax.com>
2019-04-23 07:30:15 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            LEARN SOMETHIN, FOOL!
           </a>
         </header>
       </div>
